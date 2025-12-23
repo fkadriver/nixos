7be5e027f3f380e24d78fed4a5eec9a989fd853e
Scott Jensen <fkadriver@gmail.com>
2025-12-23 14:21:17 -0600

    Updating flakes

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
   inputs = {
     nixpkgs.url = "flake:nixpkgs/nixpkgs-unstable";
     home-manager.url = "flake:home-manager";
+    nixos-hardware.url = "flake:nixos-hardware";
   };
   outputs = inputs:
     let
@@ -11,9 +12,6 @@
       };
     in
     {
-      homeConfigurations = {
-        scott = import ./homeConfigurations/scott.nix flakeContext;
-      };
       nixosConfigurations = {
         latitude-nixos = import ./nixosConfigurations/latitude-nixos.nix flakeContext;
       };
@@ -21,6 +19,7 @@
         common = import ./nixosModules/common.nix flakeContext;
         desktop = import ./nixosModules/desktop.nix flakeContext;
         latitude-nixos-hardware = import ./nixosModules/latitude-nixos-hardware.nix flakeContext;
+        syncthing = import ./nixosModules/syncthing.nix flakeContext;
         tailscale = import ./nixosModules/tailscale.nix flakeContext;
         user-scott = import ./nixosModules/user-scott.nix flakeContext;
       };
