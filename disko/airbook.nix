18c9291e699c73777ea237795976c26d72b22e73
Scott Jensen <fkadriver@gmail.com>
2026-01-03 10:00:13 -0600

    Added encrptedSwap and swap to airbook 	modified:   disko/airbook.nix

--- disko/airbook.nix
+++ disko/airbook.nix
@@ -19,12 +19,28 @@
               };
             };
             root = {
-              size = "100%";
+              size = "-20G";
               content = {
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
                 mountOptions = [ "defaults" "noatime" ];
+              };
+            };
+            encryptedSwap = {
+              size = "100M";
+              content = {
+                type = "swap";
+                randomEncryption = true;
+                priority = 100; # prefer to encrypt as long as we have space for it
+              };
+            };
+            plainSwap = {
+              size = "100%";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hiberation from this device
               };
             };
           };
